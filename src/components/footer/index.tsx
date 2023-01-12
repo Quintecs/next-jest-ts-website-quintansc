dfe9ba65b64e0caac611fb3203baccc6ad93789f
Gustavo Quintans <gustavosantos.2095@gmail.com>
2023-01-12 04:47:52 -0300

    fix(footer): construção do escopo do footer e ajustando estilos do portifólio

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,18 +3,16 @@ import { Container, textAlign } from "@mui/system";
 import Link from "next/link";
 import { GridColumn } from "./style";
 
-const style = {
- display: "flex", 
- justifyContent: "center",
- textAlign: "center"
-}
-
 const Footer = () => {
   return (
     <footer style={{ borderColor: "rgba(255, 255, 255, 0.2)", height: "100%" }}>
       <Container style={{ padding: "20px" }}>
         <Grid container spacing={2}>
-          <Grid container xs={12} style={style}>
+          <Grid container xs={12} style={{
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center"
+          }}>
             <Grid item xs={3}>QuintansTec</Grid>
             <Grid item xs={3}>Newsletter</Grid>
             <Grid item xs={3}>Explore</Grid>
